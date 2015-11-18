7b9752006819436f1b022f55cf437295cdbfcd6d
peteraaser@gmail.com <peteraaser@gmail.com>
2015-11-18 12:55:42 +0100

    Removed obsolete files

--- src/main/scala/Daisy/Daisy.scala
+++ src/main/scala/Daisy/Daisy.scala
@@ -13,7 +13,7 @@ object DaisyMain {
         val kernel_dim = 3
         val rows = kernel_dim
         val cols = kernel_dim*kernel_dim
-        // chiselMainTest(args, () => Module(new Tile(img_width, control_data_width, pixel_data_width, HDMI_data_width, cols, rows))) { c => new TileTest(c) }
+        chiselMainTest(args, () => Module(new Tile(img_width, control_data_width, pixel_data_width, HDMI_data_width, cols, rows))) { c => new TileTest(c) }
 
         // chiselMainTest(args, () => Module(new PixelGrid(24, 9, 3))) { c => new PixelGridTest(c) }
 
@@ -31,7 +31,7 @@ object DaisyMain {
         
         // processor
         
-        chiselMainTest(args, () => Module(new Processor(24, 9, 3, 3))) { c => new ProcessorRunTest(c) }
+        // chiselMainTest(args, () => Module(new Processor(24, 9, 3, 3))) { c => new ProcessorRunTest(c) }
 
         // chiselMainTest(args, () => Module(new Processor(24, 9, 3, 3))) { c => new ProcessorInitTest(c) }
 
